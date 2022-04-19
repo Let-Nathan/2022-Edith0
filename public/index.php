e082282cade8882f55cb2da97f08e7898c4aa19d
LEA-RASOLOARISON <lea.rasoloarison@gmail.com>
2022-04-19 11:33:00 +0200

    aside bar skeleton

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 if (getenv('ENV') === false) {
     require_once __DIR__ . '/../config/debug.php';
-    require_once __DIR__ . '/../config/db.php';
+    require_once __DIR__ . '/../config/db.php.dist';
 }
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
