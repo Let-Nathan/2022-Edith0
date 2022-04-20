f6414cd381d02171992ffea2da3184dc55be5d7b
Giuseppe Petraroli <g.petraroli@gmail.com>
2022-04-20 08:42:57 +0200

    before merging with dev

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Model\CommentsManager;
+use App\Model\DocumentsManager;
+use App\Model\LikesPostsManager;
 use App\Model\PostManager;
+use App\Model\UsersManager;
 
 class HomeController extends AbstractController
 {
@@ -12,10 +16,22 @@ class HomeController extends AbstractController
     public function index(): string
     {
         $postManager = new PostManager();
+        $userManager = new UsersManager();
+        $commentsManager = new CommentsManager();
+        $likesPostManager = new LikesPostsManager();
+        $documentsManager = new DocumentsManager();
 
         $posts = $postManager->selectAll('last_update_datetime', 'DESC');
 
+        foreach ($posts as $i => $post) {
+            $posts[$i]['user'] = $userManager->selectOneById($post['users_id']);
+            $posts[$i]['comments'] = $commentsManager->selectByPostId($post['id']);
+            $posts[$i]['nLikes'] = $likesPostManager->countPostLikes($post['id']);
+            $posts[$i]['documents'] = $documentsManager->getByPostId($post['id']);
+        }
 
-        return $this->twig->render('Home/index.html.twig', ['data' => $posts]);
+        return $this->twig->render('Home/index.html.twig', [
+            'posts' => $posts,
+            ]);
     }
 }
