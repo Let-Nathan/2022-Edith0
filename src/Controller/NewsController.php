4fd59cf07759ee57878d43c4455b990b01142944
LEA-RASOLOARISON <lea.rasoloarison@gmail.com>
2022-04-23 15:02:53 +0200

    maj news

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,14 +2,29 @@
 
 namespace App\Controller;
 
+use App\Model\LikesNewsManager;
 use App\Model\NewsManager;
+use App\Model\UsersManager;
 
-class NewsController extends AbstractController{
+class NewsController extends AbstractController
+{
+    /**
+     * Display news
+     */
 
-    public function showNews(): string{
+    public function showNews(): string
+    {
 
         $newsManager = new NewsManager();
+        $userManager = new UsersManager();
+        $likesNewsManager = new LikesNewsManager();
+
         $news = $newsManager->selectAll();
+
+        foreach ($news as $i => $new) {
+            $news[$i]['user'] = $userManager->selectOneById($new['user_id']);
+            $news[$i]['nLikes'] = $likesNewsManager->countNewsLikes($new['id']);
+        }
         return $this->twig->render('News/show.html.twig', ['news' => $news]);
     }
 }
