d090542fcf1f61e525abafeff674a9b5d3bd7898
Let-nathan <letournel.nathan@gmail.com>
2022-04-28 10:58:01 +0200

    WIP on eLearning

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -10,12 +10,12 @@ class LearningController extends AbstractController
     public function list(): string
     {
         $learningManager = new LearningManager();
-        $learningContentsManager = new LearningContentsManager();
+        $contentsManager = new LearningContentsManager();
         // Display Learning
         $learning = $learningManager->selectAll('id', 'ASC');
         // Show content of each Learning catégories
         foreach ($learning as $index => $value) {
-            $learning[$index]['content'] = $learningContentsManager->selectContentId($value['id']);
+            $learning[$index]['content'] = $contentsManager->selectContentId($value['id']);
         }
         return $this->twig->render('Learning/categories.html.twig', ['learning' => $learning]);
     }
